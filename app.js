a89e9cb0a25ae2795f954e6ccc4e505dee6c3770
Helmer Lauronen <helmer.lauronen@gmail.com>
2025-05-03 02:37:59 +0300

    Fix broken health check

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 5000
 
 app.get('/health', (req, res) => {
   // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
   res.send('ok')
 })
 
