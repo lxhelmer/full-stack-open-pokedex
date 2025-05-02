c5d79abbb10ac30d2be6bf3a9bf4a2f0cea2a673
Helmer Lauronen <helmer.lauronen@gmail.com>
2025-05-03 01:04:40 +0300

    Add build script

--- app.js
+++ app.js
@@ -4,9 +4,14 @@ const app = express()
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
+app.get('/version', (req, res) => {
+  res.send('1')
+})
+
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   // eslint-disable-next-line
   console.log(`server started on port ${PORT}`)
 })
+
